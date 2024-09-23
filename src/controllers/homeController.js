65a44fc44260ba79f108d82c91a49863377b9525
Present111 <22521118@gm.uit.edu.vn>
2024-09-23 23:12:24 +0700

    design list user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -14,30 +14,45 @@ const getPresent111 = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     let email = req.body.email;
     let name = req.body.myname;
     let city = req.body.city;
 
-    connection.query(
+    // connection.query(
+    //     `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`, // Add a comma after this line
+    //     [email, name, city],
+    //     function (err, results) {
+    //         if (err) {
+    //             console.error(err);
+    //             return res.status(500).send('An error occurred while creating the user');
+    //         }
+    //         console.log(results);
+    //         res.send('User created successfully');
+    //     }
+    // );
+
+    let [results, fields] = await connection.query(
         `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`, // Add a comma after this line
         [email, name, city],
-        function (err, results) {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('An error occurred while creating the user');
-            }
-            console.log(results);
-            res.send('User created successfully');
-        }
     );
 
-    console.log(email, name, city);
+    console.log(">>>check results: ", results);
+    res.send('User created successfully');
+
+    //console.log(email, name, city);
+
+    // const [results, fields] = await connection.query('select * from Users u');
+}
+
+const getCreatePage = (req, res) => {
+    res.render('create.ejs');
 }
 
 module.exports = {
     getHomepage,
     getABC,
     getPresent111,
-    postCreateUser
+    postCreateUser,
+    getCreatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getHomepage } = require('../controllers/homeController')
 const { getABC } = require('../controllers/homeController')
-const { getPresent111, postCreateUser } = require('../controllers/homeController')
+const { getPresent111, postCreateUser, getCreatePage } = require('../controllers/homeController')
 const router = express.Router();
 
 // router.Method('/route',handler)
@@ -10,8 +10,10 @@ router.get('/', getHomepage);
 
 router.get('/abc', getABC);
 
-router.get('/present111', getPresent111)
+router.get('/present111', getPresent111);
 
-router.post('/create-user', postCreateUser)
+router.post('/create-user', postCreateUser);
+
+router.get('/create', getCreatePage);
 
 module.exports = router;
